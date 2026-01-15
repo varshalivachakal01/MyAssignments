587065399bec2da2e64b416afea8f9cb608e4fe3
varshalivachakal01 <varshalivachakal01@gmail.com>
2026-01-16 01:05:41 +0530

    Add files via upload
    Week 1/Day 1

---
+++ 01-Primitive-Datatypes.js
@@ -0,0 +1,19 @@
+let firstName = "ASRI"
+console.log(firstName);
+console.log(typeof firstName); 
+
+let companyName = "nASDHJ"
+console.log(companyName);
+console.log(typeof companyName); 
+
+let mobileNumber = 12333333
+console.log(mobileNumber);
+console.log(typeof mobileNumber); 
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation); 
+
+let hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright); 
